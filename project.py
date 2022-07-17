0468c7f920c794879b7d311e341be36d4d91a690
Vedant Bhasin <vedantbhasin@Vedants-MacBook-Pro.local>
2022-07-17 19:27:22 +0530

    updated clean data function to remove duplicates and convert time columns to datetime datatypes. Also added more columns which should be dropped from the dataset

--- project.py
+++ project.py
@@ -33,17 +33,28 @@ def load_data(train_file: str, test_file: str, rider_file: str):
     return (train, test, riders)
 
 
-def clean_data(df: pd.DataFrame, cols_to_drop: list):
-    """takes in a pandas.DataFrame object and a list of columns to remove 
-    removes the respective columns inplace 
-
+def clean_data(df: pd.DataFrame, cols_to_drop: list, time_cols: list):
+    """takes in a pandas.DataFrame object, list of columns to remove and a list
+    of column names with time data. Does the following:
+        1.removes duplicate rows in the data
+        2.converts the time columns of the data to pandas.datetime64 objects  
+        3.rounds the time to the nearest hour 
+        4.removes the respective columns inplace
+    
     Arguments:
         df (pandas.DataFrame): dataframe from which columns will be removed
         cols_to_drop (list): list of column names to be dropped
+        time_cols (list): list of column names with time data to be converted
     
     Returns:
         None
     """
+    df.drop_duplicates(inplace=True)
+    
+    for time_col in time_cols:
+        df[time_col] = (pd.to_datetime(df[time_col]))
+        df[time_col] = df[time_col].dt.round('1H')
+
     df.drop(columns=cols_to_drop, inplace=True)
     return
 
@@ -58,7 +69,7 @@ def merge_dataframes(standard_data: pd.DataFrame, rider_data: pd.DataFrame):
     Returns:
         pandas.DataFrame: merged dataframe where each row contains the standard
         features and the corresponding rider features
-    """    
+    """
     return pd.merge(standard_data,rider_data,on="Rider Id")
 
 def main():
@@ -72,21 +83,43 @@ def main():
     train_cols_to_drop = ['Order No', 'User Id', 'Vehicle Type', 
                         'Arrival at Destination - Day of Month',
                         'Arrival at Destination - Weekday (Mo = 1)',
-                        'Arrival at Destination - Time']
-    test_cols_to_drop = ['Order No', 'User Id', 'Vehicle Type']
+                        'Arrival at Destination - Time',
+                        'Placement - Day of Month',
+                        'Placement - Weekday (Mo = 1)',
+                        'Placement - Time',
+                        'Confirmation - Day of Month', 
+                        'Confirmation - Weekday (Mo = 1)',
+                        'Confirmation - Time']
+
+    test_cols_to_drop = ['Order No', 'User Id', 'Vehicle Type',
+                        'Placement - Day of Month',
+                        'Placement - Weekday (Mo = 1)',
+                        'Placement - Time',
+                        'Confirmation - Day of Month', 
+                        'Confirmation - Weekday (Mo = 1)',
+                        'Confirmation - Time',
+                        'Arrival at Pickup - Time']
     riders_cols_to_drop = []
 
+    # time columns remaining which need to be converted to datetime objects
+    time_cols = ['Pickup - Time']
+
     # removing columns from the dataframes
-    clean_data(train, train_cols_to_drop)
-    clean_data(test, test_cols_to_drop)
-    clean_data(riders, riders_cols_to_drop)
+    clean_data(train, train_cols_to_drop, time_cols)
+    clean_data(test, test_cols_to_drop, time_cols)
+    clean_data(riders, riders_cols_to_drop, [])
 
     # merging train and test dataframes with rider data
     m_train = merge_dataframes(train, riders)
     m_test = merge_dataframes(test, riders)
 
+ 
+    print(f"m_train: \n{m_train.columns}")
 
-    print(f"PROCESSED DATA: \ntrain:\n{m_train.head()}\ntest:\n{m_test.head()}")
+    sns.relplot(data=m_train, x='Age', y='Time from Pickup to Arrival', hue='Rider Id')
+    sns.relplot(data=m_train, x='No_Of_Orders', y='Time from Pickup to Arrival', hue='Rider Id')
+    sns.relplot(data=m_train, x='No_of_Ratings', y='Time from Pickup to Arrival', hue='Rider Id')
+    plt.show()
     return
 
 
